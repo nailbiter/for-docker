4032abd86bdede282b74eec59bf73a440accd523
Alex Leontiev <nailbiter@dtws-work.in>
2021-10-16 22:10:59 +0900

    server

--- complex/server/index.js
+++ complex/server/index.js
@@ -18,8 +18,44 @@ const pgClient = new Pool({
   password: keys.pgPassword,
   port: keys.pgPort
 });
-pgClient.on("connect",()=>{
+pgClient.on("connect", () => {
   client
     .query("CREATE TABLE IF NOT EXISTS values(number INT)")
-    .catch((err)=>console.error(err));
+    .catch(err => console.error(err));
+});
+
+// redis client setup
+const redis = require("redis");
+const redisClient = redis.createClient({
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000
+});
+const redisPublisher = redisClient.duplicate();
+
+// express route handlers
+app.get("/", (req, res) => {
+  res.send("Hi");
+});
+app.get("/values/all", async (req, res) => {
+  const values = await pgClient.query("select * from values");
+  res.send(values.rows);
+});
+app.get("/values/current", async (req, res) => {
+  redisClient.hgetall("values", (err, values) => {
+    res.send(values);
+  });
+});
+app.post("/values/", async (req, res) => {
+  const index = req.body.index;
+  if (parseInt(index) > 40) {
+    return res.status(422).send("Index too high");
+  }
+  redisClient.hset("values", index, "Nothing yet!");
+  redisPublisher.publish("insert", index);
+  pgClient.query("insert into values(number) values($1)", [index]);
+  res.send({ working: true });
+});
+app.listen(5000, err => {
+  console.log("listening");
 });
